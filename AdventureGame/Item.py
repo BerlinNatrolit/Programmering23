3ccff550bb20ebc0fbe94663ab54bf4c294c4b50
Jimmy <berlin.jimmy@gmail.com>
2023-10-04 13:46:13 +0200

    Added comments to Item

--- AdventureGame/Item.py
+++ AdventureGame/Item.py
@@ -16,17 +16,22 @@ class Item:
     def getDescription(self):
         return self.description
     
+    # return the total damage with current DoT:s (Damage over time)
+    # Total damage is weapon base damage as well as all dot-damage.
     def getDamage(self):
         totalDamage = self.baseDamage+self.dotDamage*self.dotTime
         return totalDamage
     
+    # Set a new damage on the weapon
     def setDamage(self, newDamage):
         self.baseDamage = newDamage
-        
+     
+    # Add a DoT to the weapon
     def setDot(self, newDotDamage, newDotTime):
         self.dotDamage = newDotDamage
         self.dotTime = newDotTime
-        
+    
+    # Critical hits double the total damage.
     def criticalHit(self):
         return self.getDamage()*2
 
